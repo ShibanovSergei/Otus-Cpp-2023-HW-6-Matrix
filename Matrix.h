62b6e1f75f65feebc40bbcbd556e90b4b96674c9
Sergei.Shibanov <sergei.shibanov@aspose.com>
2024-02-19 10:46:54 +0500

    Initial implementation

--- Matrix.h
+++ Matrix.h
@@ -1,5 +1,68 @@
-template<typename T>
+#include <map>
+
+template<typename T, T undefValue>
 class Matrix
 {
+	using FullIndex = std::pair<unsigned int, unsigned int>;
 
+	std::map<FullIndex, T> _values;
+	T _undefValue;
+	FullIndex _lastUsedIndex;
+
+public:
+	Matrix();
+	unsigned Size();
+	//Matrix& operator() (unsigned int row, unsigned int col);
+	Matrix<T, undefValue>& operator() (unsigned int row, unsigned int col);
+	Matrix<T, undefValue>& operator= (const T newValue);
+	operator T();	// Introduced for Matrix<T, undefValue>& operator= (const T newValue); call
+
+	auto begin() { return std::begin(_values); }
+	auto end() { return std::end(_values); }
 };
+
+template<typename T, T undefValue>
+inline Matrix<T, undefValue>::Matrix()
+{
+	_undefValue = undefValue;
+}
+
+template<typename T, T undefValue>
+inline unsigned Matrix<T, undefValue>::Size()
+{
+	return _values.size();
+}
+
+template<typename T, T undefValue>
+Matrix<T, undefValue>& Matrix<T, undefValue>::operator()(unsigned int row, unsigned int col)
+{
+	_lastUsedIndex = std::make_pair(row, col);
+	return *this;
+}
+
+template<typename T, T undefValue>
+inline Matrix<T, undefValue>& Matrix<T, undefValue>::operator=(const T newValue)
+{
+	if (newValue == _undefValue)
+	{
+		if (_values.contains(_lastUsedIndex))
+		{
+			_values.erase(_lastUsedIndex);
+		}
+	}
+	else	
+		_values[_lastUsedIndex] = newValue;
+	
+	return *this;
+}
+
+template<typename T, T undefValue>
+inline Matrix<T, undefValue>::operator T()
+{
+	if (_values.contains(_lastUsedIndex))
+		return _values[_lastUsedIndex];
+	else
+		return _undefValue;
+}
+
+
--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
-﻿#include "main.h"
+﻿#include <assert.h>
+#include <tuple>
+
+#include "main.h"
 #include "Matrix.h"
+
 
 using namespace std;
 
@@ -7,6 +11,34 @@ int main()
 {
 	cout << "Hi all from CMake." << endl;
 
-	//Matrix<int, -1> matrix;
+	CheckAsExplained();
+
 	return 0;
+}
+
+void CheckAsExplained()
+{
+	Matrix<int, -1> matrix;
+
+	auto a = matrix(0, 0);
+	assert(a == -1);
+	assert(matrix.Size() == 0);
+
+	matrix(100, 100) = 314;
+	assert(matrix(100, 100) == 314);
+	assert(matrix.Size() == 1);
+
+	//for (auto c : matrix)
+	//{
+	//	int x, y, v;
+	//	std::tie(x, y, v) = c;
+	//	std::cout << x << y << v << std::endl;
+	//}
+
+	matrix(10, 1) = 2;
+	matrix(1, 10) = 3;
+	assert(matrix(10, 1) == 2);
+	assert(matrix(1, 10) == 3);
+	assert(matrix(10, 10) == -1);
+	assert(matrix.Size() == 3);
 }
